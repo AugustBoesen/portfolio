631ed31c31cd302da8a62abd25efa38699f9abee
AugustBoesen <augustboesen@gmail.com>
2023-06-29 12:49:58 +0300

    updated readme & added GitHub link to about section

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+## Running the project
 
 First, run the development server:
 
@@ -18,17 +18,11 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## About file structure
+All content generally resides in the "src" folder.
+1. "app" folder contains the layout and the home page.
+2. "components" folder contains dynamic content and everything in the home page, demonstrating component usage in building websites.
+3. "img" folder is self explanatory, containing images used.
+4. "pages" folder contains the other 3 pages and a "_app.tsx" for multi-page metadata.
+5. "styles" folder contains globals.css and possible additional css files.
+The project uses TailWind [Next.js](https://tailwindcss.com/) for demonstrative purposes, utilizing the framework for faster workflow outcomes.
--- src/pages/biography.tsx
+++ src/pages/biography.tsx
@@ -5,6 +5,7 @@ import {earthbg1} from "@/img"
 import Image from "next/image"
 import '../styles/globals.css'
 import Head from "next/head";
+import Link from 'next/link'
 
 export default function biography() {
   return (
@@ -24,16 +25,19 @@ export default function biography() {
           <div className='px-5'>
             <h2 className='text-3xl mt-2'>About me</h2>
             <p className='text-left pt-2'>Outside of work, I have an advanced degree in street dance studies and have continued dancing as a hobby.
-            Alongside several sub-genres of street dance and basics in contemporary & ballet, I have studied capoeira.</p>
+            Alongside several sub-genres of street dance and basics in contemporary & ballet, I have also studied capoeira.</p>
             <p  className='text-left pt-2'>I have a passion for all things nerdy, including boffering, minifigure painting and TTRPGs.</p>
-            <p className='text-left pt-2'>I have also done video editing and 3D rendering, including solo producing a music video for my instrumental song "vapauta". I have a Filmora license and have used it for video editing.</p>
-            <h2 className='text-2xl mt-4'>GitHub Repository</h2>
-            <p className='text-left pt-2'>You can freely inspect the code behind this website. Everything was made by yours truly for demonstrative purposes.</p>
+            <p className='text-left pt-2'>I have also done video editing and 3D rendering, including solo producing a music video for my instrumental 
+            song "vapauta". I have a Filmora license and have used it for video editing.</p>
+            <h2 className='text-2xl mt-4'>Website repository</h2>
+            <p className='text-left pt-2 mb-2'>You can freely inspect the code behind this website. Everything was made by yours truly for demonstrative purposes.</p>
+            <Link href='https://github.com/AugustBoesen/portfolio' className='linkbtn font-bold'>Link to GitHub</Link>
           </div>
           <div className='px-5'>
             <h2 className='text-3xl mt-2'>Contact me</h2>
             <p className='text-left pt-2'>You can send me an email to augustboesen@gmail.com anytime.</p>
-            <p className='text-left pt-2'>Generally I am able to begin freelance work with a week's notice and my workflow allows me to execute such orders usually in a 1-5 tracks per day, if need be. Initially I can offer 4-6 hours per week of work towards any project, depending on scale and intensity.</p>
+            <p className='text-left pt-2'>Generally I am able to begin freelance work with a week's notice and my workflow allows me to execute such orders usually 
+            in a 1-5 tracks per day, if need be. Initially I can offer 4-6 hours per week of work towards any project, depending on scale and intensity.</p>
           </div>
         </div>
       </div>
